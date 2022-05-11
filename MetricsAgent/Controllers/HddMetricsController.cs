5a5704836ad6d8a6ffbb467c0bf032c7cde9634a
romanisanin <romanisanin@gmail.com>
2022-05-10 23:18:52 -0400

    Bug Fixing

--- MetricsAgent/Controllers/HddMetricsController.cs
+++ MetricsAgent/Controllers/HddMetricsController.cs
@@ -4,12 +4,12 @@ using System;
 
 namespace MetricsAgent.Controllers
 {
-    [Route("api/hdd")]
+    [Route("api/metrics/hdd")]
     [ApiController]
     public class HddMetricsController : ControllerBase
     {
         // d. api / metrics / hdd / left / from /{ fromTime}/ to /{ toTime}
-        [HttpGet("/left/from/{fromTime]/to/{toTime}")]
+        [HttpGet("/left/from/{fromTime}/to/{toTime}")]
         public IActionResult GetResult(
             [FromRoute] TimeSpan fromTime, TimeSpan toTime)
         {
